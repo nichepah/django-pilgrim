024600a1a9e4af8d6f5d7e123f1153c93942d7c2
nichepah <nichepah@gmail.com>
2021-07-24 17:41:47 +0530

    added generic views

--- mypollsite/polls/views.py
+++ mypollsite/polls/views.py
@@ -1,65 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from polls.models import Question, Choice
-from django.template import loader
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.shortcuts import get_object_or_404, HttpResponseRedirect
+from django.views import generic
 
+
+from .models import Choice, Question
 
 # Create your views here.
 
-
-def index(request):
-	return HttpResponse("Hello, you are at the polls index.")
-
-
-# 2nd definition; here index is defined again
-def index(request):
-	latest_question_list = Question.objects.order_by('pub_date')[:5]
-	output = ', '.join([q.question_text for q in latest_question_list])
-	return HttpResponse(output)
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
 
-# 3rd definition; here index is defined again
-# this works with template
-def index(request):
-	latest_question_list = Question.objects.order_by('pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-	context = { 'latest_question_list': latest_question_list }
-	# return HttpResponse(template.render(context, request))
-	# shortcut 
-	return render(request, 'polls/index.html', context)
+	def get_queryset(self):
+		"""Return the last five published questions."""
+		return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-	return HttpResponse("You are looking at qn %s." %question_id)
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
 
 
-# 2nd definition for detail to raise 404 error
-def detail(request, question_id):
-	try:
-		question = Question.objects.get(pk=question_id)
-	except Question.DoesNotExist:
-		raise Http404("Question does not exist")
-	return render(request, 'polls/detail.html', {'question': question})
-
-
-# 3rd definition for detail to raise 404 error
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
-
-
-def results(request, question_id):
-	response = "results of question %s."
-	return HttpResponse(response %question_id)
-
-
-# 2nd results 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question': question})
+class ResultView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
