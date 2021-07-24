f48ef7e32cab708011ae24698358b81587c6231e
nichepah <nichepah@gmail.com>
2021-07-24 14:33:56 +0530

    added django app, part 2

--- mypollsite/mypollsite/urls.py
+++ mypollsite/mypollsite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('polls', include('polls.urls')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
--- mypollsite/polls/views.py
+++ mypollsite/polls/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def index(request):
 	return HttpResponse("Hello, you are at the polls index.")
+
+
+def detail(request, question_id):
+	return HttpResponse("You are looking at qn %s." %question_id)
+
+
+def results(request, question_id):
+	response = "results of question %s."
+	return HttpResponse(response %question_id)
+
+
+def vote(request, question_id):
+	return HttpResponse("You are voting on qn %s." %question_id)
+
+
