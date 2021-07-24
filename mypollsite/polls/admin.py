328761f35b71c3b98e012b766423850c59947a54
nichepah <nichepah@gmail.com>
2021-07-25 00:17:05 +0530

    added poll admin page stackedInline

--- mypollsite/polls/admin.py
+++ mypollsite/polls/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 
 # Register your models here.
 
-from polls.models import Question, Choice
+from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+	model = Choice
+	extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+	(None, 			{'fields': ['question_text']}),
+	('Date Info', 	{'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
