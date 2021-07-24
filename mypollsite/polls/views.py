abd299cb73556e3640e77cb23342964a9ecd30ce
nichepah <nichepah@gmail.com>
2021-07-24 14:59:32 +0530

    templates added, view modified

--- mypollsite/polls/views.py
+++ mypollsite/polls/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Question
+from django.template import loader
 
 
 # Create your views here.
+
 
 def index(request):
 	return HttpResponse("Hello, you are at the polls index.")
@@ -19,5 +22,23 @@ def results(request, question_id):
 
 def vote(request, question_id):
 	return HttpResponse("You are voting on qn %s." %question_id)
+
+
+# latest definition works; here index is defined again
+def index(request):
+	latest_question_list = Question.objects.order_by('pub_date')[:5]
+	output = ', '.join([q.question_text for q in latest_question_list])
+	return HttpResponse(output)
+
+
+# latest definition works; here index is defined again
+# this works with template
+def index(request):
+	latest_question_list = Question.objects.order_by('pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	return HttpResponse(template.render(context, request))
 
 
