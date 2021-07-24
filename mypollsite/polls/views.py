0253c39757a905fb3082416b4ca834e38d55557a
nichepah <nichepah@gmail.com>
2021-07-24 21:40:43 +0530

    tests, static added

--- mypollsite/polls/views.py
+++ mypollsite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 from .models import Choice, Question
@@ -14,8 +15,12 @@ class IndexView(generic.ListView):
 
 
 	def get_queryset(self):
-		"""Return the last five published questions."""
-		return Question.objects.order_by('-pub_date')[:5]
+		"""Return the last five published questions, 
+		excluding those set to be published in future
+		"""
+		return Question.objects.filter(
+			pub_date__lte=timezone.now()
+			).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
